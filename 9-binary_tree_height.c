7c62e849921688c3fd04c720f83b67c436307d93
Emma <furlan.emmanuelle@neuf.fr>
2025-04-13 02:34:58 +0200

    9-height correction

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -6,16 +6,17 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-size_t left_height, right_height;
+size_t left_height = 0;
+size_t right_height = 0;
 
 if (tree == NULL)
+return (0);
+
+if (tree->left == NULL && tree->right == NULL)
 return (0);
 
 left_height =  binary_tree_height(tree->left);
 right_height = binary_tree_height(tree->right);
 
-if (left_height > right_height)
-return (left_height + 1);
-else
-return (right_height + 1);
+return ((left_height > right_height ? left_height : right_height)+1);
 }
